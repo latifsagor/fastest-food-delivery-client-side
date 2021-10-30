f1d4d84daea75966752052c314e9f2e61d623af7
latifsagor <latif.sagor1042@gmail.com>
2021-10-30 17:19:48 +0600

    order review added

--- src/Component/Home/Services/Services.js
+++ src/Component/Home/Services/Services.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react'
+import { Button, Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import useAuth from '../../hooks/useAuth'
 import Service from '../Service/Service'
 
 const Services = () => {
@@ -8,14 +11,49 @@ const Services = () => {
       .then((res) => res.json())
       .then((data) => setServices(data))
   }, [])
+
+  const { user } = useAuth()
+
+  const handleAddToCart = (index) => {
+    const data = services[index]
+    data.email = user?.email
+
+    fetch('http://localhost:5000/addItem', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+  }
   return (
     <div id="services" className="container">
       <div className="row">
         <h2 className="pt-5 pb-2 text-center text-uppercase fw-bolder">
           Our Services
         </h2>
-        {services.map((service) => (
-          <Service key={service._id} service={service}></Service>
+        {services.map((service, index) => (
+          <div className="col-lg-4 col-md-6 col-12 mb-3">
+            <Card style={{ width: '22rem' }}>
+              <Card.Img variant="top img-fluid" src={service?.img} />
+              <Card.Body>
+                <Card.Title>{service?.name}</Card.Title>
+                <Card.Text>{service?.description}</Card.Text>
+                <Card.Text className="text-success">
+                  Price: {service?.price}$
+                </Card.Text>
+                <Link to={`/booking/${service?.id}`}>
+                  <Button variant="outline-dark">View Details</Button>
+                </Link>
+                <Button
+                  variant="outline-success ms-5"
+                  onClick={() => handleAddToCart(index)}
+                >
+                  Buy Now
+                </Button>
+              </Card.Body>
+            </Card>
+          </div>
         ))}
       </div>
     </div>
@@ -23,3 +61,7 @@ const Services = () => {
 }
 
 export default Services
+
+{
+  /* <Service key={service._id} service={service} index={index}></Service> */
+}
--- src/Component/OrderReview/OrderReview.js
+++ src/Component/OrderReview/OrderReview.js
@@ -1,11 +1,43 @@
-import React from 'react'
-import useServices from '../hooks/useServices'
+import React, { useEffect, useState } from 'react'
+import { Button, Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import useAuth from '../hooks/useAuth'
 
 const OrderReview = () => {
-  const [details, setDetails] = useServices()
+  const [orders, setOrders] = useState([])
+
+  const { user } = useAuth()
+  const email = user?.email
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/myOrders/${email}`)
+      .then((res) => res.json())
+      .then((data) => setOrders(data))
+  }, [])
+  console.log(orders)
+
   return (
-    <div>
-      <p>This is OrderReview </p>
+    <div className="container text-center">
+      <p>This is OrderReview {orders?.length} </p>
+      <div className="row">
+        {orders.map((order) => (
+          <div className="col-lg-4 col-md-6 col-12 mb-3">
+            <Card style={{ width: '22rem' }}>
+              <Card.Img variant="top img-fluid" src={order?.img} />
+              <Card.Body>
+                <Card.Title>{order?.name}</Card.Title>
+                <Card.Text>{order?.description}</Card.Text>
+                <Card.Text className="text-success">
+                  Price: {order?.price}$
+                </Card.Text>
+
+                <Button>Delete</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        ))}
+        <div className="col-lg-6"></div>
+      </div>
     </div>
   )
 }
